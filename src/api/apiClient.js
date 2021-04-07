d3f2d89cbec356cd2e40a60d2afa695933566e84
Rodney Matambo <rmatambo8@icloud.com>
2021-04-07 17:08:34 -0400

    all ready now i think?

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -3,14 +3,14 @@ const BASE_URL = ""
 export const getNotes = (callback) => {
   axios.get(BASE_URL + '/getNotes')
     .then(({data}) => {
-      return JSON.parse(data.body)
+      return data.body;
     })
     .then(({notes}) => callback(notes));
 }
 export const createNote = async (newNote, callback) => {
   const {data} = await axios.post(BASE_URL + '/createNote', newNote);
   
-  const note = JSON.parse(data.body)
+  const note = data.body;
   console.log("this is the axios front end for creating a new item:,", data)
 
   if (callback) callback(note);
@@ -19,7 +19,7 @@ export const createNote = async (newNote, callback) => {
 
 export const updateNote = async (updateNote, callback) => {
   const {data} = await axios.put(BASE_URL + '/updateNote', updateNote);
-  return JSON.parse(data.body);
+  return data.body
 }
 
 export const deleteNote = async (noteId, callback) => {
